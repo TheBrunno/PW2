9e4dc5edbf15daedfaf58da90e40a5a316e93fb3
Bruno Oliveira <68453992+TheBrunno@users.noreply.github.com>
2023-03-06 20:43:20 -0300

    hello world

---
+++ Hello world PHP/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Olá PHP</title>
+</head>
+<body>
+    <?php
+    echo '<h2 style="text-align:center; color:#fc5193;">Hello World</h2>'
+    ?>
+</body>
+</html>
