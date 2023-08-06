da96436fc84fd395b5654234f8cbb6adf1f88fa2
Westwitt <112172527+Westwitt@users.noreply.github.com>
2023-08-06 17:48:27 -0300

    bug fix
    bug fix 9/10 pontos

--- QUIZ/pages/config/cadastro.php
+++ QUIZ/pages/config/cadastro.php
@@ -2,8 +2,12 @@
     include('conexao.php');
 
     session_start();
+    $questao = $_POST["res"];
+    $_SESSION["score"] = $_SESSION["score"]+$questao;
     $user = $_SESSION['user'];
     $score = $_SESSION['score'];
+
+
 
     if(empty($user)){
         $user = "guest";
